96f4364162cfad50a85fc94362e732cfe0a06125
Victor Torres <victortorres@victortorres-VirtualBox.(none)>
2014-02-10 21:04:52 +0100

    ssl

--- ssl.rb
+++ ssl.rb
@@ -1,10 +1,13 @@
+#!/usr/bin/env ruby
+#ssl.rb
+
 require 'rubygems'
 require 'net/ssh'
 
 def run_remotely(command, host, args)
 NET::SSH.start(host, args) do |session|
 session.process.popen3(command) do |stdin,stdout,stderr|
-yield stdin,stdout,stderr
+yield stdin, stdout, stderr
 end
 end
 end
